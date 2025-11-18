f46436e09e932747e70000297efc5572eed8b69f
PiotrIT2015 <piotr.prochnicki@gmail.com>
2025-11-18 06:40:20 +0100

    Feature-fix/added screenshot

--- frontend/src/scenes/BudynekAplikacjiScene.js
+++ frontend/src/scenes/BudynekAplikacjiScene.js
@@ -1,27 +1,20 @@
 import React from 'react';
-
-// Możesz dodać trochę stylów, aby "budynek" i "drzwi" wyglądały lepiej.
-// Najlepiej umieścić je w osobnym pliku CSS, np. `BudynekAplikacjiScene.css`
-// i zaimportować go tutaj.
-import './BudynekAplikacjiScene.css'; 
+// Importujemy plik CSS tutaj. WAŻNE: to jest linia JavaScript, która informuje Webpacka o CSS.
+import './BudynekAplikacjiScene.css'; // <--- Poprawiona linia
 
 const BudynekAplikacjiScene = () => {
-  // URL do Twojej aplikacji na GitHub. Zmień go na właściwy link.
-  // Użyj linku do GitHub Pages (jeśli masz tam wersję demonstracyjną)
-  // lub po prostu do repozytorium.
-  const myAppUrl = 'https://github.com/PiotrIT2015/WW-P-sounds-help';
+  const myAppUrl = 'https://pcamobileapp.azurewebsites.net/';
 
   return (
     <div className="scene-container">
       <h1>Witaj w Centrum Aktywności!</h1>
       <p>Wybierz aplikację, z której chcesz skorzystać.</p>
-      
+
       <div className="applications-grid">
-        {/* Link do Duolingo */}
-        <a 
-          href="https://www.duolingo.com/" 
-          target="_blank" 
-          rel="noopener noreferrer" 
+        <a
+          href="https://www.duolingo.com/"
+          target="_blank"
+          rel="noopener noreferrer"
           className="app-link"
         >
           <div className="app-door">
@@ -30,13 +23,10 @@ const BudynekAplikacjiScene = () => {
           </div>
         </a>
 
-        {/* Link do Brainia */}
-        {/* Znalazłem aplikację Brainia w sklepach z aplikacjami, ale strona internetowa jest mniej oczywista. */}
-        {/* Poniżej link do oficjalnej strony, z której można pobrać aplikację. */}
-        <a 
-          href="https://logiclike.com/pl/lamiglowki/dla-doroslych" 
-          target="_blank" 
-          rel="noopener noreferrer" 
+        <a
+          href="https://logiclike.com/pl/lamiglowki/dla-doroslych"
+          target="_blank"
+          rel="noopener noreferrer"
           className="app-link"
         >
           <div className="app-door">
@@ -45,11 +35,10 @@ const BudynekAplikacjiScene = () => {
           </div>
         </a>
 
-        {/* Link do Twojej aplikacji */}
-        <a 
+        <a
           href={myAppUrl}
-          target="_blank" 
-          rel="noopener noreferrer" 
+          target="_blank"
+          rel="noopener noreferrer"
           className="app-link"
         >
           <div className="app-door my-app">
